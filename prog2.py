563294a58d8873f14eb3466bc63dd70d009148e4
DeniseIra02 <irajubilo@gmail.com>
2021-11-08 21:50:52 +0800

    remove prog2.py

--- prog2.py
+++
@@ -1,9 +0,0 @@
-def getAplOrng():
-    _apl = int(input("How many apple do you want to buy? \n> "))
-    _orng = int(input("How many orange do you want to buy? \n> "))
-    return _apl, _orng
-
-def getAplOrngPrice():
-    _aplPrice = 20 
-    _orngPrice = 25 
-    return _aplPrice, _orngPrice
