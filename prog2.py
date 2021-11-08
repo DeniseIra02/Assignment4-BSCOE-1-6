9d317417184746687b0250bf6f7299e04810effc
DeniseIra02 <irajubilo@gmail.com>
2021-11-08 16:55:25 +0800

    Start Program2

---
+++ prog2.py
@@ -0,0 +1,9 @@
+def getAplOrng():
+    _apl = int(input("How many apple do you want to buy? \n> "))
+    _orng = int(input("How many orange do you want to buy? \n> "))
+    return _apl, _orng
+
+def getAplOrngPrice():
+    _aplPrice = 20 
+    _orngPrice = 25 
+    return _aplPrice, _orngPrice
