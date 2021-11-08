e9706dfe2e1e1a8cc9ec133bd6c049a43da97977
DeniseIra02 <irajubilo@gmail.com>
2021-11-08 22:00:12 +0800

    Start Program2

---
+++ Program2.py
@@ -0,0 +1,10 @@
+def getAplOrng():
+    _apl = int(input("How many apple do you want to buy? \n> "))
+    _orng = int(input("How many orange do you want to buy? \n> "))
+    return _apl, _orng
+
+def getAplOrngPriceTotal(quantApl, quantOrng):
+    _aplPrice = quantApl * 20 
+    _orngPrice = quantOrng * 25 
+    overallTtl = _aplPrice + _orngPrice
+    return overallTtl
